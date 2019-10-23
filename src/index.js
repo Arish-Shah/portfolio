2d66e735d40338eaf6aed2c1cddb95e5bfef38c8
Arish-Shah <arish0246@gmail.com>
2019-10-23 16:32:47 +0530

    Switched to transform: translateY

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import App from './App';
-import './scss/index.scss';
+import App from './App'
+import './scss/index.scss'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-if (module.hot) {
-	module.hot.accept();
-}
+ReactDOM.render(<App />, document.getElementById('root'))
