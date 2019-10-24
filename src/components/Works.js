dea14e8ba59754d0770d5f231b9b5633b1b5ed71
Arish-Shah <arish0246@gmail.com>
2019-10-24 21:23:37 +0530

    Added Progressively loaded images

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,60 +1,82 @@
 import React, { useState } from 'react'
+import Img from 'react-image'
 
 //images import
-import christmas from '../assets/christmas.png'
-import cloud from '../assets/cloud.jpg'
-import hbs from '../assets/hbs.jpg'
-import mss from '../assets/mss.jpg'
-import page3 from '../assets/page-3.jpg'
-import page4 from '../assets/page-4.jpg'
-import page5 from '../assets/page-5.jpg'
-import photoshopWorkshop from '../assets/photoshop-workshop.jpg'
-import seasonsGreeting from '../assets/seasons-greeting.jpg'
+//LARGE
+import largeChristmas from '../assets/large/christmas.png'
+import largeCloud from '../assets/large/cloud.jpg'
+import largeHbs from '../assets/large/hbs.jpg'
+import largeMss from '../assets/large/mss.jpg'
+import largePage3 from '../assets/large/page-3.jpg'
+import largePage4 from '../assets/large/page-4.jpg'
+import largePage5 from '../assets/large/page-5.jpg'
+import largePhotoshopWorkshop from '../assets/large/photoshop-workshop.jpg'
+import largeSeasonsGreeting from '../assets/large/seasons-greeting.jpg'
+//SMALL
+import smallChristmas from '../assets/small/christmas.jpeg'
+import smallCloud from '../assets/small/cloud.jpeg'
+import smallHbs from '../assets/small/hbs.jpeg'
+import smallMss from '../assets/small/mss.jpeg'
+import smallPage3 from '../assets/small/page-3.jpeg'
+import smallPage4 from '../assets/small/page-4.jpeg'
+import smallPage5 from '../assets/small/page-5.jpeg'
+import smallPhotoshopWorkshop from '../assets/small/photoshop-workshop.jpeg'
+import smallSeasonsGreeting from '../assets/small/seasons-greeting.jpeg'
 
 const Works = () => {
 	const [selectedImage, setSelectedImage] = useState(null)
-	const [loaded, setLoaded] = useState(false)
 
 	const images = [
-		{ src: christmas, alt: 'Christmas' },
-		{ src: cloud, alt: 'Cloud' },
-		{ src: hbs, alt: 'HostBigSpace' },
-		{ src: mss, alt: 'MSS' },
-		{ src: page3, alt: 'Page 3' },
-		{ src: page4, alt: 'Page 4' },
-		{ src: page5, alt: 'Page 5' },
-		{ src: photoshopWorkshop, alt: 'Photoshop Workshop' },
-		{ src: seasonsGreeting, alt: "Season's Greetings" }
+		{ large: largeChristmas, small: smallChristmas, alt: 'Christmas' },
+		{ large: largeCloud, small: smallCloud, alt: 'Cloud' },
+		{ large: largeHbs, small: smallHbs, alt: 'HostBigSpace' },
+		{ large: largeMss, small: smallMss, alt: 'MSS' },
+		{ large: largePage3, small: smallPage3, alt: 'Page 3' },
+		{ large: largePage4, small: smallPage4, alt: 'Page 4' },
+		{ large: largePage5, small: smallPage5, alt: 'Page 5' },
+		{
+			large: largePhotoshopWorkshop,
+			small: smallPhotoshopWorkshop,
+			alt: 'Photoshop Workshop'
+		},
+		{
+			large: largeSeasonsGreeting,
+			small: smallSeasonsGreeting,
+			alt: "Season's Greetings"
+		}
 	]
 
 	const handleClick = index => {
 		setSelectedImage(images[index])
 	}
 
+	const closeModal = () => {
+		setSelectedImage(null)
+	}
+
 	return (
 		<>
 			<div className="works">
 				{images.map((image, index) => (
-					<img
+					<Img
 						key={index}
-						src={image.src}
-						alt={image.alt}
+						src={image.large}
+						loader={image.small}
 						onClick={() => handleClick(index)}
-						onLoad={() => setLoaded(true)}
 					/>
 				))}
 			</div>
-			{selectedImage && <PopUp image={selectedImage} />}
+			{selectedImage && <PopUp image={selectedImage} closeModal={closeModal} />}
 		</>
 	)
 }
 
-const PopUp = ({ image }) => {
+const PopUp = ({ image, closeModal }) => {
 	return (
-		<div className="pop-up">
+		<div className="pop-up" onClick={closeModal}>
 			<div className="backdrop">
 				<div className="modal">
-					<img src={image.src} alt={image.alt} />
+					<Img src={image.large} loader={image.small} />
 				</div>
 			</div>
 		</div>
