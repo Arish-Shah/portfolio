11d647ab353bfed7c8006c1a158602cb8450f53b
Arish-Shah <arish0246@gmail.com>
2019-10-25 16:22:28 +0530

    Placeholder check

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import ProgressiveImage from 'react-progressive-image'
+import React, { useState } from 'react'
 
 //images import
 //LARGE
@@ -12,6 +11,7 @@ import largePage4 from '../assets/large/page-4.jpg'
 import largePage5 from '../assets/large/page-5.jpg'
 import largePhotoshopWorkshop from '../assets/large/photoshop-workshop.jpg'
 import largeSeasonsGreeting from '../assets/large/seasons-greeting.jpg'
+
 //SMALL
 import smallChristmas from '../assets/small/christmas.jpg'
 import smallCloud from '../assets/small/cloud.jpg'
@@ -24,46 +24,66 @@ import smallPhotoshopWorkshop from '../assets/small/photoshop-workshop.jpg'
 import smallSeasonsGreeting from '../assets/small/seasons-greeting.jpg'
 
 const Works = () => {
-	document.body.style.overflow = 'auto'
-
 	const images = [
-		{ large: largeChristmas, small: smallChristmas, alt: 'Christmas' },
-		{ large: largeCloud, small: smallCloud, alt: 'Cloud' },
-		{ large: largeHbs, small: smallHbs, alt: 'HostBigSpace' },
-		{ large: largeMss, small: smallMss, alt: 'MSS' },
-		{ large: largePage3, small: smallPage3, alt: 'Page 3' },
-		{ large: largePage4, small: smallPage4, alt: 'Page 4' },
-		{ large: largePage5, small: smallPage5, alt: 'Page 5' },
+		{
+			large: largeChristmas,
+			small: smallChristmas,
+			alt: 'Christmas Sale',
+			loaded: false
+		},
+		{
+			large: largeCloud,
+			small: smallCloud,
+			alt: 'Cloud Hosting',
+			loaded: false
+		},
+		{ large: largeHbs, small: smallHbs, alt: 'HostBigSpace', loaded: false },
+		{
+			large: largeMss,
+			small: smallMss,
+			alt: 'Microsoft Student Society',
+			loaded: false
+		},
+		{ large: largePage3, small: smallPage3, alt: 'Page 3', loaded: false },
+		{ large: largePage4, small: smallPage4, alt: 'Page 4', loaded: false },
+		{ large: largePage5, small: smallPage5, alt: 'Page 5', loaded: false },
 		{
 			large: largePhotoshopWorkshop,
 			small: smallPhotoshopWorkshop,
-			alt: 'Photoshop Workshop'
+			alt: 'Photoshop Workshop',
+			loaded: false
 		},
 		{
 			large: largeSeasonsGreeting,
 			small: smallSeasonsGreeting,
-			alt: "Season's Greetings"
+			alt: "Season's Greetings",
+			loaded: false
 		}
 	]
 
 	return (
-		<>
-			<div className="works">
-				{images.map((image, index) => (
-					<div className="work-container" key={index}>
-						<ProgressiveImage src={image.large} placeholder={image.small}>
-							{(src, loading) => (
-								<img
-									style={{ filter: loading ? 'blur(20px)' : 'none' }}
-									src={src}
-									alt=""
-								/>
-							)}
-						</ProgressiveImage>
-					</div>
-				))}
+		<div className="works">
+			{images.map((image, index) => (
+				<Work key={index} details={image} />
+			))}
+		</div>
+	)
+}
+
+const Work = ({ details }) => {
+	const [loaded, setLoaded] = useState(false)
+
+	return (
+		<div className="work">
+			<div className="image-placeholder">
+				<img
+					src={details.large}
+					onLoad={() => setLoaded(true)}
+					alt={details.alt}
+					style={{ opacity: loaded ? '1' : '0' }}
+				/>
 			</div>
-		</>
+		</div>
 	)
 }
 
