41385d460fed5488bc5c2b0f1dd66a638b066df3
Arish-Shah <arish0246@gmail.com>
2019-10-27 15:35:20 +0530

    Changed Works color

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,25 +1,7 @@
 import React from 'react'
-
-//Image imports
-import christmas from '../images/christmas.jpeg'
-import cloud from '../images/cloud.jpeg'
-import hbs from '../images/hbs.jpeg'
-import mss from '../images/mss.jpeg'
-import page2 from '../images/page-2.jpeg'
-import photoshop from '../images/photoshop-workshop.jpeg'
-import season from '../images/season.jpeg'
+import { images } from '../content/works'
 
 const Works = () => {
-	const images = [
-		{ src: christmas, alt: 'Christmas: HostBigSpace' },
-		{ src: cloud, alt: 'Cloud Hosting: HostBigSpace' },
-		{ src: hbs, alt: 'Wordpress Hosting: HostBigSpace' },
-		{ src: mss, alt: 'Microsoft Student Society' },
-		{ src: page2, alt: 'Synactics, Al Saqr' },
-		{ src: photoshop, alt: 'Hands-on Workshop: Photoshop' },
-		{ src: season, alt: "Season's Greetings: HostBigSpace" },
-	]
-
 	return (
 		<div className="works">
 			{images.map((image, index) => (
---
+++ src/content/works.js
@@ -0,0 +1,53 @@
+//Image imports
+import christmas from '../images/christmas.jpeg'
+import cloud from '../images/cloud.jpeg'
+import hbs from '../images/hbs.jpeg'
+import mss from '../images/mss.jpeg'
+import page2 from '../images/page-2.jpeg'
+import photoshop from '../images/photoshop-workshop.jpeg'
+import season from '../images/season.jpeg'
+
+export const images = [
+	{
+		src: christmas,
+		alt: 'Christmas: HostBigSpace',
+		link:
+			'https://drive.google.com/file/d/1siLqCJyDUQJvo6jBgeLRc6QmqxEyzbJd/view',
+	},
+	{
+		src: cloud,
+		alt: 'Cloud Hosting: HostBigSpace',
+		link:
+			'https://drive.google.com/file/d/1eFo7hILjvJd4L2knqQ_8eBrGaCqcsq0G/view',
+	},
+	{
+		src: hbs,
+		alt: 'Wordpress Hosting: HostBigSpace',
+		link:
+			'https://drive.google.com/file/d/1DAu4GKVOF_l5_gB9J3oaq0qBv3xXFddp/view',
+	},
+	{
+		src: mss,
+		alt: 'Microsoft Student Society',
+		link:
+			'https://drive.google.com/file/d/1ROxi0XK17_usxBkp-1Ba0w7HN1ZOZLP4/view',
+	},
+	{
+		src: page2,
+		alt: 'Synactics, Al Saqr',
+		link:
+			'https://drive.google.com/file/d/1kWguQKwvAJ7ZiRxirC7ZZA0tiDq3j3MG/view',
+	},
+	{
+		src: photoshop,
+		alt: 'Hands-on Workshop: Photoshop',
+		link:
+			'https://drive.google.com/file/d/1u3YcvenBxYZMSIY76Hp3SujXZLKNOBJt/view',
+	},
+	{
+		src: season,
+		alt: "Season's Greetings: HostBigSpace",
+		link:
+			'https://drive.google.com/file/d/1kYbWlcpoIdtIgtneif0q2Fle2eTQQjIZ/view',
+	},
+]
